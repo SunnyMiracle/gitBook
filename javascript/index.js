05e2f65847e0fecbbd2a1a5754d62a6b1cf7a70f
SunnyMiracle <2674565374@qq.com>
2020-06-23 20:24:05 +0800

    作用域与变量提升

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,26 @@
-function foo() {
-    const fn = new Function('name', 'a = 10; console.log(name);');
-    fn('123')
-    console.log(a);
-}
+// function foo() {
+//     const fn = new Function('name', 'a = 10; console.log(name);');
+//     fn('123')
+//     console.log(a);
+// }
+// foo();
+
+// for (var i = 0; i < 10; i++) {
+//     //
+// }
+// console.log(i);
+
+// console.log(b);
+// var b = 10;
+
 foo();
+var foo;
+function foo() {
+    console.log(1);
+}
+foo = function () {
+    console.log(2);
+}
+// function foo() {
+//     console.log(3);
+// }
